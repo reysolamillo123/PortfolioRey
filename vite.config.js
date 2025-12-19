2cbe4fde78bf03acb2ff5dda2568fe223c005fb3
reysolamillo123 <solamillo.rey@dnsc.edu.ph>
2025-12-19 19:02:53 +0800

    Change base path to root for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/PortfolioRey/", // REPLACE THIS with your exact repo name on GitHub
+  base: "/", // Changed to root for Netlify
 })
