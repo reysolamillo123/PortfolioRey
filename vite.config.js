b266122de3597f192f6e394b04b9620cd244a4fc
reysolamillo123 <solamillo.rey@dnsc.edu.ph>
2025-12-19 18:53:59 +0800

    Restore base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/PortfolioRey/", // REPLACE THIS with your exact repo name on GitHub
 })
